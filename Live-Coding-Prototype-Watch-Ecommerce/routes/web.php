b830137669507c9e298d4d271ef6001a7511a6d5
Dina Dehbi <147596316+dinadehbi@users.noreply.github.com>
2025-02-07 13:24:54 +0100

    Delete Live-Coding-Prototype-Watch-Ecommerce directory

--- Live-Coding-Prototype-Watch-Ecommerce/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
-
-Route::get('/products', [ProductController::class, 'index'])->name('products.index'); // Show products page
-Route::post('/products', [ProductController::class, 'store'])->name('products.store'); // Store a new product
-Route::delete('/products/{id}', [ProductController::class, 'destroy'])->name('products.destroy'); // Delete a product
